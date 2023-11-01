96f91291e46c4f069c3472ddffabf4c1e1397955
khanian <khanylee@gmail.com>
2023-11-01 18:07:37 +0900

    flutter stateful example 1

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,60 @@ class ConstraintWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        ExampleStateless(),
+        ExampleStateful(),
+      ]
+    );
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  const ExampleStateful({super.key});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  int index = 0;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (index == 5) {
+              index = 0;
+              return;
+            }
+
+            index++;
+            // print(index);
+          });
+        },
+        child: Container(
+          color: Colors.blue.withOpacity(index/5),
+          child: Center(
+            child: Text('$index'),
+          ),
+        ),
+      ),
+    );
   }
 }
